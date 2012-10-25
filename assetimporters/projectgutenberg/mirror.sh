14d6c6e19834157e4428d28681078ad9d855df57
Aaron Seigo <aseigo@kde.org>
2012-10-25 11:16:09 +0200

    process the catalog too

--- assetimporters/projectgutenberg/mirror.sh
+++ assetimporters/projectgutenberg/mirror.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+# set up our destination
 mkdir -p ../../server/content/gutenberg
-rsync -aHS -vv --delete --delete-after --include='*/' --include='*.epub' --include='*.pdf' --include='*.jpg' --exclude='*' --prune-empty-dirs ftp.ibiblio.org::gutenberg-epub ../../server/content/gutenberg
-./coverProcessing.sh ../../server/content/gutenberg ../../server/public/images
 
+# rsync the content
+rsync -aHS -vv --delete --delete-after --include='*/' --include='*.epub' --include='*.pdf' --include='*.jpg' --exclude='*' --prune-empty-dirs ftp.ibiblio.org::gutenberg-epub ../../server/content/gutenberg
+
+# now populate the database
+rm catalog.rdf
+wget http://www.gutenberg.org/feeds/catalog.rdf.bz2
+bunzip2 catalog.rdf.bz2
+gutimport catalog.rdf ../../server/content/gutenberg
+
+# process the cover images
+./coverProcessing.sh ../../server/content/gutenberg ../../server/public/images
