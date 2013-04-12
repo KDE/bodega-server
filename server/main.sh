fa9faf24cd267498accb3dfa2280f6de5389f6e4
Aaron Seigo <aseigo@kde.org>
2013-04-12 13:55:03 +0200

    slightly more helpful main.sh

--- server/main.sh
+++ server/main.sh
@@ -1,2 +1,5 @@
-#NODE_ENV=production node app.js
-NODE_ENV=test node app.js
+#!/bin/sh
+case "$1" in
+    "production" ) NODE_ENV=production forever start app.js -l bodega.log -o bodega-out.log -e bodega-error.log -w &;;
+               * ) NODE_ENV=test node app.js;;
+esac
