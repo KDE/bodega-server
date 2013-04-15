2dee4c55f20df66dbb1f5b10966bd774b39712eb
Aaron Seigo <aseigo@kde.org>
2013-04-15 14:06:20 +0200

    IT LIVES! :) basic upload works rather nicely now

--- server/lib/db/upload.js
+++ server/lib/db/upload.js
@@ -15,33 +15,102 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
+var errors = require('../errors.js');
 var utils = require('../utils.js');
 var fs = require('fs');
 
-module.exports = function(db, req, res) {
-    var json = {
-        device : req.session.user.device,
-        authStatus : req.session.authorized,
-        points : req.session.user.points,
-        asset : 1
-    };
+function checkPartner(db, req, res)
+{
+    console.log("checking " + req.body.partner + ' ' + req.session.user.id);
+    var partner = req.body.partner;
+    if (partner === null) {
+        db.query("select partner from affiliations a left join personRoles r on (a.role = r.id) where a.person = $1 and r.description = 'Content Creator';",
+                [req.session.user.id],
+                function(err, qRes) {
+                    if (err || !qRes.rows || !qRes.rows.size < 1) {
+                        errors.report('UserNotUploader', req, res);
+                        return;
+                    }
 
+                    req.body.partner = qRes.rows[0].partner;
+                    fn(db, req, res);
+                });
+    } else {
+                        console.log("checking up on partner");
+        db.query("select partner from affiliations a left join personRoles r on (a.role = r.id) where a.partner = $1 and a.person = $2 and r.description = 'Content Creator';",
+                [req.body.partner, req.session.user.id],
+                function(err, result) {
+                    if (err || !result.rows || !result.rows.size < 1) {
+                        console.log("didn't find partner");
+                        errors.report('UploadPartnerInvalid', req, res);
+                        return;
+                    }
+
+                    storeAsset(db, req, res);
+                });
+    }
+}
+
+function storeAsset(db, req, res)
+{
+    console.log("storing asset...");
+    db.query("select nextval('seq_assetsids') as assetId;", [],
+             function(err, result) {
+                 req.files.asset.id = result.rows[0].assetid;
+                 app.assetStore.upload(req,
+                     function(err, result) {
+                         if (err) {
+                             console.log("error due to bad rename?");
+                             errors.report('Upload failed', req, res);
+                             return;
+                         }
+
+                         req.files.asset.incomingPath = result.path;
+                         recordAsset(db, req, res);
+                     });
+            });
+}
+
+function recordAsset(db, req, res)
+{
+    var file = req.files.asset;
+    var incomingPath = file.incomingPath;
+    console.log(req.session.user.id, "our paths are => " + file.filename + ' ' + incomingPath);
+    var newAssetQuery = 'insert into incomingAssets (author, name, file, path) values ($1, $2, $3, $4)';
+    db.query(newAssetQuery, [req.body.partner, file.filename, file.filename, incomingPath],
+            function(err, result) {
+                if (err) {
+                    errors.report('Database', req, res, err);
+                    return;
+                }
+
+                var json = {
+                    device : req.session.user.device,
+                    authStatus : req.session.authorized,
+                    points : req.session.user.points,
+                    asset : req.files.asset.id
+                };
+
+                res.json(json);
+            });
+};
+
+module.exports = function(db, req, res) {
     console.log("start ");
+
+    var file = req.files.asset;
+    if (!file || file['size'] < 1 || file['filename'].size < 1) {
+        //console.log("error due to bad file");
+        errors.report('Upload failed', req, res);
+        return;
+    }
+
+    checkPartner(db, req, res);
+    /*
     console.log("path = "  + req.files.asset['path']);
     console.log("size = "  + req.files.asset['size']);
-    console.log("filename = "  + req.files.asset['filename']);
+    console.log("filename = "  + req.files.asset.filename);
     console.log("length = "  + req.files.asset['length']);
-    var file = req.files.asset;
-
-
-    console.log('==============================================================');
-    var stream = fs.createReadStream(req.files.asset['path']);
-    /*
-    stream.setEncoding('utf8');
-    stream.on('start', function() { console.log('==============================================================') ; })
-          .on('data', function(chunk){ console.log(chunk); })
-          .on('end', function() { console.log('==============================================================');});
-    fs.unlink(file.path);
     */
-    res.json(json);
-};
+}
+
