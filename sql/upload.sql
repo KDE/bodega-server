7454592065cf518c806f8becff7ca0cdfe79a938
Aaron Seigo <aseigo@kde.org>
2013-05-14 14:10:45 +0200

    incoming bits

---
+++ sql/upload.sql
@@ -0,0 +1,22 @@
+create table incomingAssets
+(
+    id          int         primary key default nextval('seq_assetsIds'),
+    license     int         references licenses(id),
+    author      int         references partners(id),
+    basePrice   int         not null default 0 CHECK(basePrice >= 0),
+    name        text        not null,
+    description text,
+    version     text        not null default '',
+    versionTs   timestamp   not null default (current_timestamp AT TIME ZONE 'UTC'),
+    path        text        not null,
+    file        text        not null,
+    image       text,
+    publish     bool        not null default false
+);
+
+create table incomingAssetTags
+(
+    asset       int         references incomingAssets(id) on delete cascade,
+    tag         int         references tags(id) on delete cascade
+);
+
