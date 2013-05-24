KDE/bodega-server

commit e9e5744d02fda29cf8885af97fdec78690d8116e

auth now sends emails for the reactivation of the disabled accounts
REVIEW: 110117

server/lib/db/register.js
@@ -1,4 +1,4 @@
-/* 
+/*
     Copyright 2012 Coherent Theory LLC
 
     This program is free software; you can redistribute it and/or
@@ -18,8 +18,6 @@
 var utils = require('../utils.js');
 var errors = require('../errors.js');
 var BCrypt = require('bcrypt');
-var nodemailer = require('nodemailer');
-
 
 function deleteUser(db, userId)
 {
@@ -31,89 +29,6 @@ function deleteUser(db, userId)
                 return;
             }
         });
-}
-
-function sendConfirmationEmail(db, req, res, args)
-{
-    var service = app.config.service;
-    //XXX: replace with jade and html emails
-    var template =
-'Welcome to Make·Play·Live, \n\
- \n\
- To start using your Make·Play·Live account please confirm your email address\n\
- by clicking on the following link:\n \
- \n\
- http://#{host}#{prefix}register/confirm?code=#{code}&email=#{email}&id=#{userId}\n \
- \n\
- Thank You,\n \
- Make·Play·Live Team\n';
-    var transport = nodemailer.createTransport("SMTP",{
-        host:             service.smtp.host,
-        secureConnection: service.smtp.useSSL,
-        port:             service.smtp.port,
-        auth: {
-            user: service.smtp.user,
-            pass: service.smtp.pass
-        }
-    });
-
-    var mailOptions = {
-        transport: transport, // transport method to use
-        from: service.email,
-        to: args.email, // list of receivers
-        subject: "Activate your new account", // Subject line
-        //text: text, //set later
-        //html: "<b>Hello world!</b>" // html body
-    };
-
-    var query =
-        'select ct_createAccountActivationCode($1) as activationcode;';
-
-    db.query(
-        query, [args.userId],
-        function(err, result) {
-            var text;
-            var json = {
-                userId: args.userId
-            };
-            if (err) {
-                deleteUser(db, args.userId);
-                errors.report('Database', req, res, err);
-                return;
-            }
-
-            var confirmationCode = result.rows[0].activationcode;
-            text = template.replace('#{code}', confirmationCode);
-            text = text.replace('#{email}', args.email);
-            text = text.replace('#{userId}', args.userId);
-            text = text.replace('#{host}', req.headers.host);
-            text = text.replace('#{prefix}', app.config.prefix);
-            mailOptions.text = text;
-
-            if (app.settings.env === 'test') {
-                json.confirmationCode = confirmationCode;
-                json.text = text;
-                res.json(json);
-                console.log(text);
-            } else {
-                nodemailer.sendMail(mailOptions, function(error) {
-                    var json = {};
-                    if (error) {
-                        deleteUser(db, args.userId);
-                        errors.report('MailerFailure', req, res, error);
-                        transport.close();
-                        return;
-                    }
-
-                    json.message =
-                        "Confirmation email sent!";
-                    //console.log("Message sent!");
-                    res.json(json);
-                    transport.close(); // lets shut down the connection pool
-                });
-            }
-        }
-    );
 }
 
 function createUser(db, req, res, args)
@@ -150,7 +65,7 @@ function createUser(db, req, res, args)
                         return;
                     }
                     args.userId = result.rows[0].id;
-                    sendConfirmationEmail(db, req, res, args);
+                    utils.sendConfirmationEmail(db, req, res, args.userId, args.email);
                 }
             );
         });
@@ -176,14 +91,16 @@ function findUser(db, req, res, args)
                     return;
                 }
                 args.userId = result.rows[0].id;
-                sendConfirmationEmail(db, req, res, args);
+                var sendConfirmationEmailResult = utils.sendConfirmationEmail(db, req, res, args.userId, args.email);
+                if (!sendConfirmationEmailResult) {
+                    deleteUser(db, args.userId);
+                }
             } else {
                 createUser(db, req, res, args);
             }
         }
     );
 }
-
 
 module.exports = function(db, req, res) {
     var args = {
@@ -207,3 +124,4 @@ module.exports = function(db, req, res) {
 
     findUser(db, req, res, args);
 };
+

server/lib/utils.js
@@ -1,4 +1,4 @@
-/* 
+/*
     Copyright 2012 Coherent Theory LLC
 
     This program is free software; you can redistribute it and/or
@@ -14,6 +14,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
+
+var nodemailer = require('nodemailer');
 
 module.exports.findImagePaths = function(req)
 {
@@ -63,4 +65,88 @@ module.exports.standardJson = function(req, success)
     json.success = typeof success !== 'undefined' ? success : true;
     return json;
 };
+
+module.exports.sendConfirmationEmail= function(db, req, res, userId, userEmail)
+{
+    var service = app.config.service;
+    //XXX: replace with jade and html emails
+    var template =
+'Welcome to Make·Play·Live, \n\
+ \n\
+ To start using your Make·Play·Live account please confirm your email address\n\
+ by clicking on the following link:\n \
+ \n\
+ http://#{host}#{prefix}register/confirm?code=#{code}&email=#{email}&id=#{userId}\n \
+ \n\
+ Thank You,\n \
+ Make·Play·Live Team\n';
+    var transport = nodemailer.createTransport("SMTP",{
+        host:             service.smtp.host,
+        secureConnection: service.smtp.useSSL,
+        port:             service.smtp.port,
+        auth: {
+            user: service.smtp.user,
+            pass: service.smtp.pass
+        }
+    });
+
+    var mailOptions = {
+        transport: transport, // transport method to use
+        from: service.email,
+        to: userEmail, // list of receivers
+        subject: "Activate your new account", // Subject line
+        //text: text, //set later
+        //html: "<b>Hello world!</b>" // html body
+    };
+
+    var query =
+        'select ct_createAccountActivationCode($1) as activationcode;';
+
+    db.query(
+        query, [userId],
+        function(err, result) {
+            var text;
+            var json = {
+                userId: userId
+            };
+            if (err) {
+                deleteUser(db, args.userId);
+                errors.report('Database', req, res, err);
+                return false;
+            }
+
+            var confirmationCode = result.rows[0].activationcode;
+            text = template.replace('#{code}', confirmationCode);
+            text = text.replace('#{email}', userEmail);
+            text = text.replace('#{userId}', userId);
+            text = text.replace('#{host}', req.headers.host);
+            text = text.replace('#{prefix}', app.config.prefix);
+            mailOptions.text = text;
+
+            if (app.settings.env === 'test') {
+                json.confirmationCode = confirmationCode;
+                json.text = text;
+                res.json(json);
+                console.log(text);
+            } else {
+                nodemailer.sendMail(mailOptions, function(error) {
+                    var json = {};
+                    if (error) {
+                        deleteUser(db, args.userId);
+                        errors.report('MailerFailure', req, res, error);
+                        transport.close();
+                        return false;
+                    }
+
+                    json.message =
+                        "Confirmation email sent!";
+                    //console.log("Message sent!");
+                    res.json(json);
+                    transport.close(); // lets shut down the connection pool
+                });
+            }
+        }
+    );
+    return true;
+}
 

server/lib/db/authorize.js
@@ -52,6 +52,11 @@ function authenticate(db, req, res)
             }
 
             var userData = result.rows[0];
+            if (!userData.active) {
+                utils.sendConfirmationEmail(db, req, res, userData.id, userData.email);
+                errors.report('AccountInactive', req, res);
+                return;
+            }
 
             BCrypt.compare(
                 authPassword, userData.password,

server/lib/db/confirm.js
@@ -1,4 +1,4 @@
-/* 
+/*
     Copyright 2012 Coherent Theory LLC
 
     This program is free software; you can redistribute it and/or
@@ -45,7 +45,16 @@ module.exports = function(db, req, res) {
             var json = {
                 userId: args.userId
             };
-            if (err) {
+
+            var updateError = true;
+            db.query('update people set active = true where id = $1', [args.userId], function(err2, result) {
+                if (err) {
+                    updateError = false;
+                    errors.report('Database', req, res, err2);
+                }
+            });
+
+            if (err || updateError) {
                 errors.report('Database', req, res, err);
                 res.render('registrationconfirmation.jade',
                 { layout: false,

commit 42fc660d70822811f384364db77dd75d32f99414

fix the email confirmation. The server is now able again to send emails.
This bug was introduced with the commit e9e5744d02fda29cf8885af97fdec78690d8116e

REVIEW: 110627

server/lib/db/register.js
@@ -19,18 +19,6 @@ var utils = require('../utils.js');
 var errors = require('../errors.js');
 var BCrypt = require('bcrypt');
 
-function deleteUser(db, userId)
-{
-    var query = 'DELETE FROM people WHERE id=$1;';
-    db.query(
-        query, [userId],
-        function(err, result) {
-            if (err) {
-                return;
-            }
-        });
-}
-
 function createUser(db, req, res, args)
 {
     var query =
@@ -93,7 +81,7 @@ function findUser(db, req, res, args)
                 args.userId = result.rows[0].id;
                 var sendConfirmationEmailResult = utils.sendConfirmationEmail(db, req, res, args.userId, args.email);
                 if (!sendConfirmationEmailResult) {
-                    deleteUser(db, args.userId);
+                    utils.deleteUser(db, args.userId);
                 }
             } else {
                 createUser(db, req, res, args);

server/lib/utils.js
@@ -16,6 +16,7 @@
 */
 
 var nodemailer = require('nodemailer');
+var errors = require('./errors.js');
 
 module.exports.findImagePaths = function(req)
 {
@@ -66,6 +67,20 @@ module.exports.standardJson = function(req, success)
     return json;
 };
 
+function deleteUser(db, userId)
+{
+    var query = 'DELETE FROM people WHERE id=$1;';
+    db.query(
+        query, [userId],
+        function(err, result) {
+            if (err) {
+                return;
+            }
+        });
+}
+
+module.exports.deleteUser = deleteUser;
+
 module.exports.sendConfirmationEmail= function(db, req, res, userId, userEmail)
 {
     var service = app.config.service;
@@ -110,7 +125,7 @@ module.exports.sendConfirmationEmail= function(db, req, res, userId, userEmail)
                 userId: userId
             };
             if (err) {
-                deleteUser(db, args.userId);
+                deleteUser(db, userId);
                 errors.report('Database', req, res, err);
                 return false;
             }
@@ -132,7 +147,7 @@ module.exports.sendConfirmationEmail= function(db, req, res, userId, userEmail)
                 nodemailer.sendMail(mailOptions, function(error) {
                     var json = {};
                     if (error) {
-                        deleteUser(db, args.userId);
+                        deleteUser(db, userId);
                         errors.report('MailerFailure', req, res, error);
                         transport.close();
                         return false;
@@ -148,5 +163,5 @@ module.exports.sendConfirmationEmail= function(db, req, res, userId, userEmail)
         }
     );
     return true;
-}
+};
 