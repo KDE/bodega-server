5630a5b721b4b6ae1ea2677f55c22ddba90537f5
Aaron Seigo <aseigo@kde.org>
2013-05-26 18:37:22 +0200

    move the callback call elsewhere

--- server/test/support/http.js
+++ server/test/support/http.js
@@ -21,10 +21,11 @@ function getUrl(app, url, fn, cookie) {
         res.on("end", function(chunk) {
             try {
                 res.body = JSON.parse(buf);
-                fn(res);
             } catch (e) {
                 console.log("!!!! JSON Parsing failed on this return: " + buf + "\n(" + e + ")");
             }
+
+            fn(res);
         });
     });
 }
