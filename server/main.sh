e494da9e3f489cd147341000a0bdf88b13ab98b4
Aaron Seigo <aseigo@kde.org>
2013-05-29 18:35:15 +0200

    use --production; fix log params.
    hey, we have logging now!

--- server/main.sh
+++ server/main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 case "$1" in
-    "production" ) NODE_ENV=production forever start app.js -l bodega.log -o bodega-out.log -e bodega-error.log -w &;;
-               * ) NODE_ENV=test node app.js;;
+    "production" ) forever start -aw -l bodega.log -o bodega-out.log -e bodega-error.log app.js --production&;;
+               * ) node app.js;;
 esac
