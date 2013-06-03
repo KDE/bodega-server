1424d4c0106f83e9eaa0d37c485b655ee7ce7338
Aaron Seigo <aseigo@kde.org>
2013-06-03 18:34:17 +0200

    more deadweight gone

--- assetimporters/kdeartwork-wallpapers/wallpapers-channel.sql
+++
@@ -1,4 +0,0 @@
-
-insert into channels values(default, 1, null, 'default/wallpaper.png', 'Wallpapers', 'Wallpapers', true, 0, null )
-insert into storeChannels select c.last_value, 'VIVALDI-1' from seq_channelids c;
-
