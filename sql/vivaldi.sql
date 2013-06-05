862ea7c3a7d8b31d2c5a5bb890aad09e1e4fc2d2
Aaron Seigo <aseigo@kde.org>
2013-06-05 12:24:33 +0200

    not testing data

--- sql/vivaldi.sql
+++ sql/vivaldi.sql
@@ -1,6 +1,7 @@
 --begin;
 
-insert into partners (name, developer, distributor) values ('KDE', true, true);
+insert into partners (id, name, developer, distributor, homepage, supportEmail)
+       values (1, 'Make Play Live', true, true, 'http://makeplaylive.com', 'support@makeplaylive.com');
 
 insert into stores (id, partner, name, description)
        values ('VIVALDI-1', 0, 'Vivaldi', 'Plasma Active tablet from Make Play Live');
