ed8ba1c48a787f9a1188b69d292da544d300b56d
Aaron Seigo <aseigo@kde.org>
2013-06-05 11:11:53 +0200

    the correct id

--- sql/vivaldi.sql
+++ sql/vivaldi.sql
@@ -3,6 +3,6 @@
 insert into partners (name, developer, distributor) values ('KDE', true, true);
 
 insert into stores (id, partner, name, description)
-       values (0, 0, 'Vivaldi', 'Plasma Active tablet from Make Play Live');
+       values ('VIVALDI-1', 0, 'Vivaldi', 'Plasma Active tablet from Make Play Live');
 
 --end;
