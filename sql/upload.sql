f57e6b04925f2161cdd53f4bc71958efce8d84b0
Aaron Seigo <aseigo@kde.org>
2013-06-23 16:07:31 +0200

    get rid of stale assets when the partner goes away

--- sql/upload.sql
+++ sql/upload.sql
@@ -2,7 +2,7 @@ create table incomingAssets
 (
     id          int         primary key default nextval('seq_assetsIds'),
     license     int         references licenses(id),
-    partner     int         references partners(id),
+    partner     int         references partners(id) on delete cascade,
     basePrice   int         default 0 CHECK(basePrice >= 0),
     name        text,
     description text,
