d4930d29a5eec811aa87d65ea81ecb97b4dbf9bb
Giorgos Tsiapaliokas <terietor@gmail.com>
2013-07-01 18:13:21 +0300

    move the ratings procedure and trigger from core.plsql to ratings.sql

--- sql/ratings.sql
+++ sql/ratings.sql
@@ -24,3 +24,20 @@ create table assetRatingAverages
     rating    float      not null check (rating > 0 AND rating < 6)
 );
 
+CREATE OR REPLACE FUNCTION ct_checkTagForRating() RETURNS TRIGGER AS $$
+DECLARE
+    tagId int;
+BEGIN
+    SELECT INTO tagID t.id from tags t INNER JOIN tagtypes ttype ON (t.type = ttype.id) WHERE ttype.type = 'assetType' AND t.id = NEW.assetType;
+    IF NOT FOUND THEN
+        RAISE EXCEPTION '!!!!!!!!!!!The tag must have assetType as tagtype!!!!!!!!!!!!!';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+DROP TRIGGER IF EXISTS trg_ct_checkTagForRating ON ratingAttributes;
+CREATE TRIGGER trg_ct_checkTagForRating BEFORE INSERT OR UPDATE ON ratingAttributes
+FOR EACH ROW EXECUTE PROCEDURE ct_checkTagForRating();
+
