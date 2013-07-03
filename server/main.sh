ce692d121d9339784f309217eaa8dfa3108d4a9d
Aaron Seigo <aseigo@kde.org>
2013-07-03 11:13:45 +0200

    don't watch for file changes
    this results in forever rather aggressively restarting whenever things
    like cover images change .. eventually this leads to forever dumping out

--- server/main.sh
+++ server/main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 case "$1" in
-    "production" ) forever start -aw -l bodega.log -o bodega-out.log -e bodega-error.log app.js --production&;;
+    "production" ) forever start -a -l bodega.log -o bodega-out.log -e bodega-error.log app.js --production&;;
                * ) node app.js;;
 esac
