e758f2c7860e20d6e7c0c2fd122b56d49d7a82c3
Aaron Seigo <aseigo@kde.org>
2013-07-30 07:12:35 +0200

    a rather more straight forward approach; performance is fine

--- server/lib/db/updates.js
+++ server/lib/db/updates.js
@@ -32,74 +32,56 @@ module.exports = function(db, req, res)
         return;
     }
 
-    db.query("create temporary table temporary_updates (asset int, recvd timestamp)", [],
-             function(err) {
-                if (err) {
-                    errors.report('Database', req, res, err);
-                    return;
-                }
+    var sql = 'select distinct a.id from assets a where ';
+    var rows = [];
+    var params = [];
+    var count = 0;
+    req.body.assets.forEach(function(asset) {
+        if (!Array.isArray(asset) || asset.length !== 2) {
+            return;
+        }
 
-                var sql = 'insert into temporary_updates (asset, recvd) VALUES ';
-                var rows = [];
-                var params = [];
-                var count = 0;
-                req.body.assets.forEach(function(asset) {
-                    if (!Array.isArray(asset) || asset.length !== 2) {
-                        return;
-                    }
+        var id = utils.parseNumber(asset[0]);
+        if (id < 1) {
+            return;
+        }
 
-                    var id = utils.parseNumber(asset[0]);
-                    if (id < 1) {
-                        return;
-                    }
+        var ts = Date.parse(asset[1]);
+        if (ts) {
+            rows.push('(a.id = $' + (++count) + ' and a.versionts > $' + (++count) + ')');
+            params.push(id);
+            var date = new Date();
+            date.setTime(ts);
+            params.push(date);
+        }
+    });
 
-                    var ts = Date.parse(asset[1]);
-                    if (ts) {
-                        rows.push('($' + (++count) + ', $' + (++count) + ')');
-                        params.push(id);
-                        var date = new Date();
-                        date.setTime(ts);
-                        params.push(date);
-                    }
-                });
+    if (rows.length < 1) {
+        var json = utils.standardJson(req);
+        json.assets = [];
+        res.json(json);
+        return;
+    }
 
+    sql += rows.join(' OR ');
 
-                if (rows.length < 1) {
-                    var json = utils.standardJson(req);
-                    json.assets = [];
-                    res.json(json);
-                    return;
-                }
+    //console.log(sql);
+    //console.log(params);
+    db.query(sql, params,
+    function(err, result) {
+        if (err) {
+            errors.report('Database', req, res, err);
+            return;
+        }
 
-                sql += rows.join(', ');
+        var json = utils.standardJson(req);
+        json.assets = [];
+        for (var i = 0; i < result.rowCount; ++i) {
+            json.assets.push(result.rows[i].id);
+        }
 
-                //console.log(sql);
-                //console.log(params);
-                db.query(sql, params,
-                         function(err) {
-                             if (err) {
-                                 errors.report('Database', req, res, err);
-                                 return;
-                             }
-
-                             db.query("select distinct a.id from assets a join temporary_updates u \
-                                       on (a.id = u.asset and a.versionts > u.recvd)", [],
-                                 function(err, result) {
-                                     if (err) {
-                                         errors.report('Database', req, res, err);
-                                         return;
-                                     }
-
-                                     var json = utils.standardJson(req);
-                                     json.assets = [];
-                                     for (var i = 0; i < result.rowCount; ++i) {
-                                         json.assets.push(result.rows[i].id);
-                                     }
-
-                                     res.json(json);
-                                 });
-                         });
-             });
+        res.json(json);
+    });
 };
 
 
