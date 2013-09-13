2bce34eb979ae8ff18a5219769faa442c948ea55
Aaron Seigo <aseigo@kde.org>
2013-09-13 14:21:40 +0200

    message handler for account activation

--- server/lib/messengers/participant_accountActivation.js
+++ server/lib/messengers/participant_accountActivation.js
@@ -7,7 +7,7 @@ var Url = require('url');
 module.exports.sendEmail = function(transport, db, record, cb)
 {
     db.query('select ct_createAccountActivationCode($1) as activationcode;',
-             [ record.recipient ]
+             [ record.recipient ],
              function(err, result) {
                 if (err || result.rowCount < 1) {
                     return;
@@ -16,20 +16,20 @@ module.exports.sendEmail = function(transport, db, record, cb)
                 var mailOptions = {
                     transport: transport,
                     from: app.config.service.email,
-                    to: result.rows[0].email,
-                    subject: "Activate your new " + app.config.warehouse.name + " account"
+                    to: record.email,
+                    subject: "Activate your new " + app.config.warehouseInfo.name + " account"
                 };
 
-                record.warehouse = app.config.warehouse.name;
-                var url = Url.parse(app.config.warehouseInfo.url + app.config.prefix + "register/confirm");
+                record.warehouse = app.config.warehouseInfo.name;
+                var url = Url.parse(app.config.externalBaseUrl + app.config.prefix + "register/confirm");
                 url.query = { 'code': result.rows[0].activationcode,
-                               'email': record.email,
-                               'id': record.recipient };
+                              'email': record.email,
+                              'id': record.recipient };
                 record.url = Url.format(url);
 
                 template.createBodies(
                     {
-                        text: path.join(__dirname, '/participant_accountActivation.handlebars.text');
+                        text: path.join(__dirname, '/participant_accountActivation.handlebars.text')
                     },
                     record,
                     function(err, output) {
