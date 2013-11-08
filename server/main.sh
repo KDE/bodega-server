c21dd8a77c1808f648693865a5c3515772902b40
Aaron Seigo <aseigo@kde.org>
2013-11-08 11:11:30 +0100

    add a staging option

--- server/main.sh
+++ server/main.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 case "$1" in
     "production" ) forever start -a -l bodega-production.log -o bodega-out-production.log -e bodega-error-production.log app.js --production&;;
-               * ) node app.js;;
+    "staging" ) forever start -a -l bodega-test.log -o bodega-out-test.log -e bodega-error-test.log app.js --test&;;
+    * ) node app.js;;
 esac
